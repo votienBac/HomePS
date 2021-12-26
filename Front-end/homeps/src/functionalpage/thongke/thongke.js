0255c07501a1570795987d58b4b4bd312f51f633
Hanh Ngô <38860847+hanh-nd@users.noreply.github.com>
2021-12-26 16:05:45 +0700

    add thongke

--- Front-end/homeps/src/functionalpage/thongke/thongke.js
+++ Front-end/homeps/src/functionalpage/thongke/thongke.js
@@ -1,7 +1,8 @@
+import BarChart from "./revenuachart"
 export default function ThongKe(){
     return(
         <div>
-            <h2>trang thong ke</h2>
+            <BarChart/>
         </div>
     )
 }
