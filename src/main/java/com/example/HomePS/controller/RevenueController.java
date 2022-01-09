a3db1ed9cfd2ddef39a24dec0aa670a825c85d33
LongBG <nguyenvanlonglnbg@gmail.com>
2022-01-09 07:28:07 +0700

    update Model MonthRevenue

--- src/main/java/com/example/HomePS/controller/RevenueController.java
+++ src/main/java/com/example/HomePS/controller/RevenueController.java
@@ -2,16 +2,16 @@ package com.example.HomePS.controller;
 
 import com.example.HomePS.dto.StatisticResponse;
 import com.example.HomePS.model.Daily_TurnOver;
+import com.example.HomePS.model.MonthRevenue;
 import com.example.HomePS.service.Statistical;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
+import java.time.Period;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -31,26 +31,52 @@ public class RevenueController {
         List<Daily_TurnOver> revenueList;
         switch (status) {
             case "7 days":
-                revenueList = statistical.getRevenueDay(end.minusDays(8), end.minusDays(1));
+                revenueList = statistical.getRenevueDay(end.minusDays(8), end.minusDays(1));
             case "30 days":
-                revenueList = statistical.getRevenueDay(end.minusDays(31), end.minusDays(1));
+                revenueList = statistical.getRenevueDay(end.minusDays(31), end.minusDays(1));
             default:
                 LocalDate dateBegin2 = LocalDate.parse(dateBegin, DateTimeFormatter.ofPattern("yyyy/MM/dd"));
                 if (dateEnd.equals("")) {
-                    revenueList = statistical.getRevenueDay(dateBegin2, end);
+                    revenueList = statistical.getRenevueDay(dateBegin2, end);
                 } else {
                     LocalDate dateEnd2 = LocalDate.parse(dateEnd, DateTimeFormatter.ofPattern("yyyy/MM/dd"));
-                    revenueList = statistical.getRevenueDay(dateBegin2, dateEnd2);
+                    revenueList = statistical.getRenevueDay(dateBegin2, dateEnd2);
                 }
         }
         return ResponseEntity.ok(new StatisticResponse(revenueList, statistical.getSumTurnOver(revenueList)));
     }
-      @GetMapping("/{month}")
-    public List<Daily_TurnOver> monthRevenue(@PathVariable  String month){
-            month=month+"-01";
-            LocalDate dateBegin=LocalDate.parse(month, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
-        System.out.println(statistical.getRenevueDay(dateBegin,dateBegin.plusDays(-1+dateBegin.lengthOfMonth())));
-            return statistical.getRenevueDay(dateBegin,dateBegin.plusDays(-1+dateBegin.lengthOfMonth()));
+
+      @GetMapping("/months")
+    public List<MonthRevenue> monthRevenue(@RequestParam(required = false, defaultValue = "1970-01") String monthBegin,
+                                             @RequestParam(required = false, defaultValue = "") String monthEnd){
+            monthBegin=monthBegin+"-01";
+            LocalDate timeBegin=LocalDate.parse(monthBegin, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+            LocalDate timeEnd;
+       if(monthEnd.equals("")){
+           timeEnd=LocalDate.now();
+       }else{
+           monthEnd=monthEnd+"-01";
+           timeEnd=LocalDate.parse(monthEnd, DateTimeFormatter.ofPattern("yyyy-MM-dd"));
+       }
+          int numberMonths;
+          if(timeEnd.getYear()-timeBegin.getYear()>=1){
+
+              numberMonths=-timeBegin.getMonthValue()+12+1+(timeEnd.getYear()-timeBegin.getYear()-1)*12+timeEnd.getMonthValue();
+          }else{
+              numberMonths=timeEnd.getMonthValue()-timeBegin.getMonthValue()+1;
+          }
+
+          List<MonthRevenue> listMonthRevenue=new ArrayList<>();
+          for(int i=0;i<numberMonths;i++){
+              double revenueMonth=statistical.getSumTurnOver(statistical.getRenevueDay(timeBegin,timeBegin.plusDays(-1+timeBegin.lengthOfMonth())));
+              if(revenueMonth>0) {
+                  MonthRevenue monthRevenue=new MonthRevenue(timeBegin.getYear()+"-"+timeBegin.getMonthValue(),revenueMonth);
+                  listMonthRevenue.add(monthRevenue);
+              }
+              timeBegin=timeBegin.plusDays(timeBegin.lengthOfMonth());
+
+          }
+            return listMonthRevenue;
     }
 
 }
--- src/main/java/com/example/HomePS/model/MonthRevenue.java
+++ src/main/java/com/example/HomePS/model/MonthRevenue.java
@@ -1,11 +1,16 @@
 package com.example.HomePS.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Entity;
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
+
 public class MonthRevenue {
     private String month;
     private Double revenue;
+
 }
