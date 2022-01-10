ab937ba18f230a1f3e49f5244e3265beaa71ee72
tienbaovuong <tienbaoams@gmail.com>
2022-01-10 22:02:31 +0700

    Thu gọn query thống kê

--- Front-end/homeps/src/functionalpage/thongke/thongke.js
+++ Front-end/homeps/src/functionalpage/thongke/thongke.js
@@ -31,7 +31,7 @@ export default function ThongKe(){
         <div>
             {isQueryNgay? (
             <div className="queryBox">
-                <label>Chọn ngày bắt đầu  </label>
+                <label>Begin </label>
                 <DatePicker className = 'startTime' 
                             selected={dateBegin} 
                             onChange={(date) => setDateBegin(date)} 
@@ -39,7 +39,7 @@ export default function ThongKe(){
                             minDate = {new Date("2021/12/23")}
                             maxDate={dateEnd}
                             />
-                <label>Chọn ngày kết thúc  </label>
+                <label>End </label>
                 <DatePicker className= 'endTime'
                             selected={dateEnd} 
                             onChange={(date) => setDateEnd(date)} 
@@ -47,7 +47,7 @@ export default function ThongKe(){
                             minDate={dateBegin}
                             maxDate={new Date()}
                             />
-                <label>Chọn loại thống kê  </label>
+                <label>Type </label>
                 <Select 
                     value={queryType}
                     label="Chọn loại thống kê"
@@ -62,7 +62,7 @@ export default function ThongKe(){
             )
             :(
             <div className="queryBox">
-                <label>Chọn tháng bắt đầu  </label>
+                <label>Begin </label>
                 <DatePicker className = 'startTime' 
                             selected={dateBegin} 
                             onChange={(date) => setDateBegin(date)} 
@@ -72,7 +72,7 @@ export default function ThongKe(){
                             showMonthYearPicker
                             label = 'start'
                             />
-                <label>Chọn tháng kết thúc  </label>
+                <label>End </label>
                 <DatePicker className= 'endTime'
                             selected={dateEnd} 
                             onChange={(date) => setDateEnd(date)} 
@@ -81,7 +81,7 @@ export default function ThongKe(){
                             maxDate={new Date()}
                             showMonthYearPicker
                             />
-                <label>Chọn loại thống kê  </label>
+                <label>Type </label>
                 <Select 
                     value={queryType}
                     label="Chọn loại thống kê"
