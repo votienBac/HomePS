bb2412d74b2d2eaff1007923a171ad9e0b0ebfac
tienbaovuong <tienbaoams@gmail.com>
2022-01-11 10:12:09 +0700

    query của lượt chơi kt chuyển trang đc
    của lượt chơi hiện tại chưa sửa nhưng na ná

--- Front-end/homeps/src/functionalpage/luotchoi/finishedturnlist.js
+++ Front-end/homeps/src/functionalpage/luotchoi/finishedturnlist.js
@@ -5,6 +5,8 @@ import SearchBar from './search.js'
 import formatTime from '../../utility/formattime.js'
 function FinishedTurn() {
     const navigate = useNavigate()
+    const [isQuery, setIsQuery] = useState(false)
+    const [isChangePageQuery, setChangePageQuery] = useState(false)
     const [sizePage, setSizePage] = useState(10)
     const [finishedTurns, setFinishedTurns] = useState({
         currentPage: 1,
@@ -14,12 +16,13 @@ function FinishedTurn() {
     })
     //Load the bill list
     useEffect(() => {
+        if(!isQuery)
         fetch(`https://homeps.herokuapp.com/api/bills?page=${finishedTurns.currentPage}&size=${sizePage}&status=${'paid'}`, {
             method: 'GET'
         })
             .then(res => res.json())
             .then(finishedTurns => { setFinishedTurns(finishedTurns) })
-    }, [finishedTurns.currentPage, sizePage])
+    }, [finishedTurns.currentPage, sizePage, isQuery])
     console.log(111);
     return (
         <div className="luot-choi">
@@ -31,6 +34,10 @@ function FinishedTurn() {
                 type='paid'
                 query={finishedTurns}
                 setQuery={setFinishedTurns}
+                isQuery = {isQuery}
+                setIsQuery = {setIsQuery}
+                isChangePageQuery = {isChangePageQuery}
+                setChangePageQuery = {setChangePageQuery}
                 size={sizePage}
             />
             </div>
@@ -58,14 +65,20 @@ function FinishedTurn() {
             </table>
             <div className='paging'>
                 <button
-                    onClick={() => setFinishedTurns({ ...finishedTurns, currentPage: 1 })}
+                    onClick={() => {
+                        setFinishedTurns({ ...finishedTurns, currentPage: 1 })
+                        setChangePageQuery(isQuery)
+                    }
+                }
                 >
                     {"<<"}
                 </button>
                 <button
                     onClick={() => {
-                        if (finishedTurns.currentPage > 1)
+                        if (finishedTurns.currentPage > 1){
                             setFinishedTurns({ ...finishedTurns, currentPage: finishedTurns.currentPage - 1 });
+                            setChangePageQuery(isQuery)
+                        }
                     }
                     }
                 >
@@ -73,14 +86,20 @@ function FinishedTurn() {
                 </button>
                 <button>{finishedTurns.currentPage}</button>
                 {(finishedTurns.currentPage == finishedTurns.totalPage) || <button
-                    onClick={() => setFinishedTurns({ ...finishedTurns, currentPage: finishedTurns.currentPage + 1 })}
+                    onClick={() => {
+                        setFinishedTurns({ ...finishedTurns, currentPage: finishedTurns.currentPage + 1 })
+                        setChangePageQuery(isQuery)
+                    }
+                    }
                 >
                     {finishedTurns.currentPage + 1}
                 </button>}
                 <button
                     onClick={() => {
-                        if (finishedTurns.currentPage < finishedTurns.totalPage)
+                        if (finishedTurns.currentPage < finishedTurns.totalPage){
                             setFinishedTurns({ ...finishedTurns, currentPage: finishedTurns.currentPage + 1 })
+                            setChangePageQuery(isQuery)
+                        }
                     }
 
                     }
@@ -88,7 +107,11 @@ function FinishedTurn() {
                     {">"}
                 </button>
                 <button
-                    onClick={() => setFinishedTurns({ ...finishedTurns, currentPage: finishedTurns.totalPage })}
+                    onClick={() => {
+                        setFinishedTurns({ ...finishedTurns, currentPage: finishedTurns.totalPage })
+                        setChangePageQuery(isQuery)
+                    }
+                    }
                 >
                     {">>"}
                 </button>
--- Front-end/homeps/src/functionalpage/luotchoi/search.js
+++ Front-end/homeps/src/functionalpage/luotchoi/search.js
@@ -1,19 +1,29 @@
 import React, { useState } from 'react'
 import '../../css/luotchoi.css';
-const SearchBar = ({ type, query, setQuery, size }) => {
+const SearchBar = ({ type, query, setQuery, isQuery, setIsQuery, isChangePageQuery, setChangePageQuery, size }) => {
     const [details, setDetails] = useState('')
     console.log(details);
     const handleChange = (props) => {
         setDetails(props)
-        if (props == '') handleSearch()
+        if (props == '') {
+            setIsQuery(false)
+        }
     }
     const handleKeypress = e => {
         if (e.key === 'Enter') {
             handleSearch();
         }
     };
-
+    if(isQuery && isChangePageQuery){
+        fetch(`https://homeps.herokuapp.com/api/bills/search?size=${size}&page=${query.currentPage}&query=${details}&status=${type}`, {
+            method: 'GET',
+        })
+            .then(res => res.json())
+            .then(res => setQuery(res))
+        setChangePageQuery(false)
+    }
     const handleSearch = () => {
+        setIsQuery(true);
         console.log(details);
         fetch(`https://homeps.herokuapp.com/api/bills/search?size=${size}&query=${details}&status=${type}`, {
             method: 'GET',
